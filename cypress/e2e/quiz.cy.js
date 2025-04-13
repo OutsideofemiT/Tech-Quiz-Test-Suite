00586f7cf37fb8e0ed1ddd144d79397beb3e4678
Carmen Wheeler <carmenwh33l3r@gmail.com>
2025-04-13 02:41:23 -0500

    created tests for e2e

--- cypress/e2e/quiz.cy.js
+++ cypress/e2e/quiz.cy.js
@@ -1,3 +1,19 @@
 import questions from '../fixtures/questions.json';
 
-cy.intercept('GET', '/api/questions', questions);
+
+describe('Tech Quiz E2E', () => {
+  it('should load the quiz and display the start button', () => {
+	cy.intercept('GET', '/api/questions', questions);
+	cy.visit('/');
+	cy.findByRole('button', { name: /start quiz/i }).should('exist')
+  });
+});
+
+it('should display the second question after answering the first', () => {
+	cy.findByRole('button', { name: /start quiz/i }).click();
+	cy.findByRole('button', { name: '1' }).should('exist').click();
+	cy.findByText('What is 4 + 5?').should('exist');
+  });
+  
+
+
