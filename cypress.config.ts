aeeb9f7a693ae505e2c1a5276a60249da3d79e00
Carmen Wheeler <carmenwh33l3r@gmail.com>
2025-04-14 11:49:05 -0500

    updated cypressconfig for component testing

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,11 +2,16 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://127.0.0.1:3001',
-    setupNodeEvents(on, config) {
-      // You can add plugins or reporters here if needed
-    },
-    supportFile: 'cypress/support/e2e.ts', // update if you're using .js
+    baseUrl: 'http://127.0.0.1:3001', // for your E2E tests
+    supportFile: 'cypress/support/e2e.ts',
     specPattern: 'cypress/e2e/**/*.cy.{js,ts}',
+  },
+  component: {
+    devServer: {
+      framework: 'react',
+      bundler: 'vite',
+    },
+    supportFile: 'cypress/support/component.ts',
+    specPattern: 'cypress/component/**/*.cy.{js,ts,jsx,tsx}',
   },
 });
